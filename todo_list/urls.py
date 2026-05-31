9c1fc64fe82687ee02e95cecc30e4bb248645c73
Andriy <andrijbuluk@gmail.com>
2026-05-31 09:59:14 +0200

    Added: a link to the base page

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 
+from todo_list.views import (
+    index
+)
 
+urlpatterns = [
+    path("", index, name="index"),
+]
 
 app_name = 'todo_list'
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+from todo_list.models import Task
+
+
+def index(request):
+    list_todo = Task.objects.all()
+
+    return render(request, "todo_list/index.html", context={"todo_list": list_todo})
