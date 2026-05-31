2ba2b728b4bdb607dac3782a00be3f12eb6c104e
Andriy <andrijbuluk@gmail.com>
2026-05-31 11:22:50 +0200

    Fixed a bug with bootstrap5

--- Django_Practice/settings.py
+++ Django_Practice/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'django_bootstrap5',
     "todo_list",
 ]
 
@@ -111,3 +110,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+
+STATICFILES_DIRS = (BASE_DIR / "static",)
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
