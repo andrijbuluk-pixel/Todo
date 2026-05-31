37b25eef6cd23692cad29cd5c62f95777c0ac2fc
Andriy <andrijbuluk@gmail.com>
2026-05-31 11:22:02 +0200

    Added a link to the tag

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 
 from todo_list.views import (
-    index
+    index,
+    TagList
 )
 
 urlpatterns = [
     path("", index, name="index"),
+    path("tags/", TagList.as_view(), name="tag-list"),
 ]
 
 app_name = 'todo_list'
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
 from todo_list.models import Task
 
@@ -7,3 +8,9 @@ def index(request):
     list_todo = Task.objects.all()
 
     return render(request, "todo_list/index.html", context={"todo_list": list_todo})
+
+
+class TagList(ListView):
+    model = Task
+    context_object_name = "tag_list"
+    template_name = "todo_list/tag_list.html"
